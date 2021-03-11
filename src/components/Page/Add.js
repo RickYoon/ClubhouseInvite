3c21d40fe14f29330e17c8c98335965ea3920200
RickYoon <yoon1515@gmail.com>
2021-03-11 09:12:15 +0900

    데이터 추출연동 완료

--- src/components/Page/Add.js
+++ src/components/Page/Add.js
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavBar from "../Navbar/TopNavBar";
 import Filter from "../Atom/Filter";
 import List from "../Moleclue/List";
+import axios from "axios";
 
 const Add = () => {
+  const [infos, setInfos] = useState({
+    title: "",
+    weekday: "",
+    moderators: "",
+    images: [],
+    description: "",
+    datetime: "",
+    club: "",
+  });
+
+  const {
+    title,
+    weekday,
+    moderators,
+    images,
+    description,
+    datetime,
+    club,
+  } = infos; // 비구조화 할당을 통해 값 추출
+
+  const [text, setText] = useState("");
+  function test(event) {
+    // console.log(event);
+    event.preventDefault();
+    // console.log("form go");
+  }
+
+  const onChange = (e) => {
+    setText(e.target.value);
+    console.log(text);
+  };
+
+  const onReset = () => {
+    // setText("");
+    loadUsers();
+    console.log("click button");
+  };
+
+  const loadUsers = async () => {
+    // const url = "https://www.joinclubhouse.com/event/xkL60q1y";
+    try {
+      const result = await axios({
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        url: `https://ddjw33n2b0.execute-api.ap-northeast-2.amazonaws.com/production/coopangCrawler?url=${text}`,
+        // url: `https://z5v2zc0s9i.execute-api.ap-northeast-2.amazonaws.com/production/getLifeInfo`,
+      }).then((res) => {
+        // console.log(res.data);
+        // console.log(res.data.title);
+
+        setInfos({
+          ...infos,
+          title: res.data.title,
+          weekday: res.data.weekday,
+          images: res.data.images,
+          description: res.data.description,
+          datetime: res.data.datetime,
+          club: res.data.club,
+          moderators: res.data.moderators,
+        });
+        console.log(infos);
+      });
+    } catch {
+      alert("주소를 정확하게 입력해주세요!");
+    }
+  };
+
   return (
     <div className="container mt-3">
       <br></br>
@@ -17,19 +87,20 @@ const Add = () => {
           추가하고 더 많은 사람들과 함께해요
         </h1>
         <div className="w-full border p-3 bg-white rounded-md">
-          <form>
+          <form onSubmit={test}>
             <p className="text-center" style={{ fontSize: "15px" }}>
               초대링크를 붙여넣고, 검토하기!
             </p>
-
             <input
-              type="search"
-              name="q"
               className="bg-purple-white bg-gray-200 shadow rounded border-0 p-3 w-100 mt-0"
-              placeholder="event link url"
-              id="box"
+              onChange={onChange}
+              value={text}
+              style={{ fontSize: "12px" }}
             />
-            <button className="bg-gray-500 rounded border-0 w-100 p-3 mt-3">
+            <button
+              className="bg-gray-500 rounded border-0 w-100 p-3 mt-3"
+              onClick={onReset}
+            >
               Validate
             </button>
           </form>
--- src/components/Page/Home.js
+++ src/components/Page/Home.js
@@ -5,7 +5,7 @@ import List from "../Moleclue/List";
 import axios from "axios";
 
 const Home = () => {
-  const [users, setUser] = useState([]);
+  const [users, setUser] = useState({});
 
   useEffect(() => {
     loadUsers();
@@ -13,20 +13,50 @@ const Home = () => {
   }, []);
 
   const loadUsers = async () => {
-    const result = await axios.get("http://localhost:3003/events");
-    setUser(result.data);
+    const url = "https://www.joinclubhouse.com/event/xkL60q1y";
+    const result = await axios({
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      url: `https://ddjw33n2b0.execute-api.ap-northeast-2.amazonaws.com/production/coopangCrawler?url=${url}`,
+      // url: `https://z5v2zc0s9i.execute-api.ap-northeast-2.amazonaws.com/production/getLifeInfo`,
+    }).then((res) => {
+      console.log(res.body);
+      setUser(res.body);
+      let temp = JSON.stringify(users);
+      console.log(temp);
+      // console.log(res.data);
+    });
   };
 
   return (
     <div className="container mx-auto desktop mb-2 md:mb-10">
       <Filter />
       <div className="container mt-3 m-0 p-0">
-        {users.map((user) => (
+        {/* {users.map((user) => (
           <List props={user} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
 };
 
 export default Home;
+
+// const loadItems = async () => {
+//   setIsLoading(true);
+//   await axios({
+//     method: "get",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     url: `https://nxqs16xalf.execute-api.ap-northeast-2.amazonaws.com/default/sgMarket?cat=${props.catselection}`,
+//   }).then((res) => {
+//     setIsLoading(false);
+//     var sortingField = "revenue";
+//     const newRanking = res.data.sort(function (a, b) {
+//       // 오름차순
+//       return b[sortingField] - a[sortingField];
+//       // 13, 21, 25, 44
+//     });
